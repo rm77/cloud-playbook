dee5251dc616bc09473a70674a5d9f688ac15b6f
roy <royyana@gmail.com>
2023-06-06 14:36:31 +0700

    add startup script

--- aws/web1/create-instance-by-template.sh
+++ aws/web1/create-instance-by-template.sh
@@ -11,9 +11,12 @@ aws ec2 create-launch-template \
 	--launch-template-data '{"ImageId":"ami-007855ac798b5175e","InstanceType":"t2.micro", "KeyName": "'$KEYNAME'" }'
 
 #run install script
+#untuk base64 encode content dari file startup-install-script.txt
 #base64 startup-install-script.txt > install_base64.txt
+#tidak diperlukan, karena run-instances akan meng-encode otomatis
+#akan diperlukan, jika akan mengupdate
 
 
 aws ec2 run-instances \
-    --launch-template LaunchTemplateName=my-vm-template,Version=1
-    #--user-data file:///data/web1/install_base64.txt \
+    --launch-template LaunchTemplateName=my-vm-template,Version=1 \
+    --user-data file://startup-install-script.txt
--- aws/web1/create-instance.sh
+++ aws/web1/create-instance.sh
@@ -7,5 +7,8 @@ aws ec2 run-instances \
 	--image-id ami-007855ac798b5175e \
 	--count 1 \
 	--instance-type t2.micro \
-	--key-name $KEYNAME
+	--key-name $KEYNAME \
+	--user-data file:///data/web1/startup-install-script.txt  
+	#dengan menggunakan ini, pada waktu startup, vm akan menginstall sesuai dengan script 
+
 
