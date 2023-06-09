4968b8f81f3875109caadc28257a0da60b44b493
royyana <royyana@gmail.com>
2023-06-09 13:52:44 +0700

    add microovm

---
+++ container/microvm/getrootfs.sh
@@ -0,0 +1,9 @@
+DOCKERIMAGE=ubuntu:jammy
+IMAGENAME=ubuntu_jammy
+docker export $(docker run -d $DOCKERIMAGE) -o image.tar
+
+virt-make-fs --format=qcow2 --size=+200M image.tar image_large.qcow2
+qemu-img convert image_large.qcow2 -O qcow2 $IMAGENAME.qcow2
+rm -f image_large.qcow2 image.tar
+
+
