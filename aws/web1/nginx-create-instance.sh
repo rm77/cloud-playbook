df9420336767f257618d2f041261b82dbd593a74
roy <royyana@gmail.com>
2023-06-12 16:07:29 +0700

    add nginx related

---
+++ aws/web1/nginx-create-instance.sh
@@ -0,0 +1,14 @@
+. set.sh
+
+echo $USERNAME
+echo $KEYNAME
+
+aws ec2 run-instances \
+	--image-id ami-007855ac798b5175e \
+	--count 1 \
+	--instance-type t2.micro \
+	--key-name $KEYNAME \
+	--user-data file:///data/web1/nginx-startup-install-script.txt  
+	#dengan menggunakan ini, pada waktu startup, vm akan menginstall sesuai dengan script 
+
+
